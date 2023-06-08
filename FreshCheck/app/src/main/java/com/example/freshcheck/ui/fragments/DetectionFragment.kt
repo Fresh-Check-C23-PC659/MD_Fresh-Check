ae6c164120f43c0aed384e770bcef320a76ef033
Bennyhp <bennyhp0805@gmail.com>
2023-06-08 23:46:32 +0800

    Camera & Gallery functions

--- FreshCheck/app/src/main/java/com/example/freshcheck/ui/fragments/DetectionFragment.kt
+++ FreshCheck/app/src/main/java/com/example/freshcheck/ui/fragments/DetectionFragment.kt
@@ -1,16 +1,81 @@
 package com.example.freshcheck.ui.fragments
 
+import android.Manifest
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.Intent.ACTION_OPEN_DOCUMENT
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.freshcheck.databinding.FragmentDetectionBinding
+import com.example.freshcheck.ui.activities.ViewFinderActivity
+import com.example.freshcheck.utils.rotateFile
+import java.io.File
 
 class DetectionFragment : Fragment() {
 
     private var _binding: FragmentDetectionBinding? = null
     private val binding get() = _binding!!
+
+    private var getFile: File? = null
+
+    private val REQUEST_CODE_PERMISSIONS = 1001
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+    private val galleryLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                val selectedImage: Uri? = data?.data
+                selectedImage?.let { uri ->
+                    val contentResolver = requireContext().contentResolver
+                    val takeFlags: Int = Intent.FLAG_GRANT_READ_URI_PERMISSION or
+                            Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                    contentResolver.takePersistableUriPermission(uri, takeFlags)
+
+                    val inputStream = contentResolver.openInputStream(uri)
+                    val file = File(requireContext().cacheDir, "temp_image.jpg")
+                    file.outputStream().use { outputStream ->
+                        inputStream?.copyTo(outputStream)
+                    }
+                    getFile = file
+                    binding.ivDetectionFragment.setImageBitmap(BitmapFactory.decodeFile(file.path))
+                }
+            }
+        }
+
+
+    private val cameraLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                it.data?.getSerializableExtra("picture", File::class.java)
+            } else {
+                @Suppress("DEPRECATION")
+                it.data?.getSerializableExtra("picture")
+            } as? File
+
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+
+            myFile?.let { file ->
+                rotateFile(file, isBackCamera)
+                getFile = file
+                binding.ivDetectionFragment.setImageBitmap(BitmapFactory.decodeFile(file.path))
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,9 +85,79 @@ class DetectionFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        binding.apply {
+            btnCameraDetection.setOnClickListener { startCamera() }
+            btnGallery.setOnClickListener { openGallery() }
+        }
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        return REQUIRED_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    private fun openGallery() {
+        val galleryIntent = Intent(ACTION_OPEN_DOCUMENT).apply {
+            type = "image/*"
+        }
+        galleryLauncher.launch(galleryIntent)
+    }
+
+    private fun startCamera() {
+        val cameraIntent = Intent(requireContext(), ViewFinderActivity::class.java)
+        cameraLauncher.launch(cameraIntent)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == AppCompatActivity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_CODE_CAMERA -> {
+                    val myFile = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        data?.getSerializableExtra("picture", File::class.java)
+                    } else {
+                        @Suppress("DEPRECATION")
+                        data?.getSerializableExtra("picture")
+                    } as? File
+
+                    val isBackCamera =
+                        data?.getBooleanExtra("isBackCamera", true) ?: true
+
+                    myFile?.let { file ->
+                        rotateFile(file, isBackCamera)
+                        getFile = file
+                        binding.ivDetectionFragment.setImageBitmap(BitmapFactory.decodeFile(file.path))
+                    }
+                }
+            }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+        private const val REQUEST_CODE_CAMERA = 102
     }
 
 }
---
+++ FreshCheck/app/src/main/java/com/example/freshcheck/utils/Utils.kt
@@ -0,0 +1,42 @@
+package com.example.freshcheck.utils
+
+import android.app.Application
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import com.example.freshcheck.R
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+private const val FILENAME_FORMAT = "dd-MMM-yyyy"
+
+val timeStamp: String = SimpleDateFormat(
+    FILENAME_FORMAT,
+    Locale.US
+).format(System.currentTimeMillis())
+
+fun rotateFile(file: File, isBackCamera: Boolean = false) {
+    val matrix = Matrix()
+    val bitmap = BitmapFactory.decodeFile(file.path)
+    val rotation = if (isBackCamera) 90f else -90f
+    matrix.postRotate(rotation)
+    if (!isBackCamera) {
+        matrix.postScale(-1f, 1f, bitmap.width / 2f, bitmap.height / 2f)
+    }
+    val result = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+    result.compress(Bitmap.CompressFormat.JPEG, 100, FileOutputStream(file))
+}
+
+fun createFile(application: Application): File {
+    val mediaDir = application.externalMediaDirs.firstOrNull()?.let {
+        File(it, application.resources.getString(R.string.app_name)).apply { mkdirs() }
+    }
+
+    val outputDirectory = if (
+        mediaDir != null && mediaDir.exists()
+    ) mediaDir else application.filesDir
+
+    return File(outputDirectory, "$timeStamp.jpg")
+}
